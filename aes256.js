6f49dc9838b9724db14429081b7f61270b737c47
Darshannaik484 <darshannnaik484@gmail.com>
2025-08-01 20:06:24 +0530

    Added class for all algorithms

--- aes256.js
+++ aes256.js
@@ -29,47 +29,4 @@ class AES256 {
     return result;
   }
 }
-module.exports = AES256;
-// // const res = aes256Encrypt("Hello", "5");
-// // console.log("Encrypted message is ", res);
-// // const re = aes256Decrypt(res, "5");
-// // // console.log("decrypted message is ", re);
-// // // const obj = new aes256();
-// // // const a = obj.aes256Encrypt("Hello", "5");
-// // // const b = obj.aes256Decrypt("1c1d1e1f", "5");
-// // // console.log("Encrypted message is ", a);
-// // // console.log("Decrypted message is ", b);
-
-// function aes256Encrypt(text, key) {
-//   if (!key || key.length < 1) {
-//     throw new Error("Encryption key is required");
-//   }
-//   let result = "";
-
-//   for (let i = 0; i < text.length; i++) {
-//     const charCode = text.charCodeAt(i);
-//     const keyCode = key.charCodeAt(i % key.length);
-//     const encryptedChar = charCode ^ keyCode;
-//     result += encryptedChar.toString(16).padStart(2, "0");
-//   }
-//   return result;
-// }
-
-// function aes256Decrypt(encryptedHex, key) {
-//   if (!key || key.length < 1) {
-//     throw new Error("Decryption key is required");
-//   }
-//   let result = "";
-
-//   for (let i = 0; i < encryptedHex.length; i += 2) {
-//     const byte = parseInt(encryptedHex.substr(i, 2), 16);
-//     const keyCode = key.charCodeAt((i / 2) % key.length);
-//     const decryptedChar = byte ^ keyCode;
-//     result += String.fromCharCode(decryptedChar);
-//   }
-//   return result;
-// }
-// const res = aes256Encrypt("Hello", "5");
-// console.log("Encrypted message is ", res);
-// const re = aes256Decrypt(res, "5");
-// console.log("decrypted message is ", re);
+export { AES256 };
